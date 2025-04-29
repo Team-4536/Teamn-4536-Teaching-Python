d1f99537364f2bfc1a5d1711ef8aa5a943268519
Jack S <lolbetiwontusethimuch@gmail.com>
2025-04-29 16:25:19 -0500

    continued working on the tutorial

---
+++ Common Command and Math.py
@@ -0,0 +1,23 @@
+import math
+exampleVar = "asasas"
+anotherVar = "dfdfdfdfdfdf"
+
+#There are a couple of commands that are helpful for manipulating variables and testing code
+#The first one is print()
+#It takes in strings
+print(exampleVar)
+#You can add multiple things if you seperate them with a +
+print(exampleVar + anotherVar)
+#use the run button to see this in the terminal
+#make three strings and print them in one print statement
+
+
+
+#Next there are the math commands
+#To use math you need to import it at the top of the file like in this one
+#In order to use them you type math. and then select the one you want out of the list of options that will pop up
+#They can set something to a value like this
+pi = math.pi
+#Other examples are
+asd = math.sin(15)
+#make 4 variables and give the variables values using different math commands.
