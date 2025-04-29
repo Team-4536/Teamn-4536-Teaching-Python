c242e2b6ace3ee7c46bcaffb4203c009e0c3fc16
Jack S <lolbetiwontusethimuch@gmail.com>
2025-04-29 08:26:33 -0500

    Started making an introduction to python

---
+++ Variables.py
@@ -0,0 +1,49 @@
+#Variables
+
+#Variables are made using the syntax variable name = value
+#example
+name = "value"
+#There are four main types of variables: 
+#Booleans are True/False
+#Strings are lenghths of text
+#Ints are integers that go to 64 bit precision
+#Floats are decimals that go to 64 bit precision
+#We use camel case to name variables which is capitilise the first letter of each word past the first
+#Like this camelCaseExample = 5
+#Here are some examples of variables
+booleanExample = True 
+stringExample = "example"
+intExample = 123456789
+floatExample = 1.23456789
+#Now can you make an int called myInt with the value 197 and a string called myString with the text "I made a string!" 
+
+
+
+
+
+#Manipulating Variables
+#There are a few ways to change variables 
+#The first is using = to set it to something new
+#Like this
+booleanExample = False
+#This works for all variables
+
+#For all int, floats, and string you  can also do variable = variable + value
+stringExample = stringExample + " yay"
+#Lastly for ints and floats you can do variable += value
+intExample += 5
+#the + in += can be replace with -,*, or / in order to to subtraction, multiplication, and division
+#Now can you set myString to "wo"
+
+#Now add "rd" to myString
+
+#Now add 50 to myInt using +=
+
+
+
+#Now make 5 variables with different names and manipulate each of them at least once and 3 of them twice:
+
+
+
+
+
